7317e40197fbfa3989c76a5efd7bfbd95c979390
Eric Harris-Braun <eric@harris-braun.com>
2017-02-18 15:15:02 -0500

    moved Register call in testing to it's own file
    keep running into problems with testing because it doesn't run
    early enough, so now it's in: a_test.go

---
+++ a_test.go
@@ -0,0 +1,10 @@
+package holochain
+
+import (
+	"testing"
+)
+
+// needed to setup the holochain environment, not really a test.
+func Test(t *testing.T) {
+	Register()
+}
--- entry_test.go
+++ entry_test.go
@@ -7,11 +7,6 @@ import (
 	"testing"
 )
 
-// needed to setup the holochain environment, not really a test.
-func Test(t *testing.T) {
-	Register()
-}
-
 func TestGob(t *testing.T) {
 	g := GobEntry{C: mkTestHeader("myData")}
 	v, err := g.Marshal()

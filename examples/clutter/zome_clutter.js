3bd600ab987f79c843b57b02bffe474e879c17bd
Eric Harris-Braun <eric@harris-braun.com>
2017-03-06 20:48:18 -0500

    clutter wip

--- examples/clutter/zome_clutter.js
+++ examples/clutter/zome_clutter.js
@@ -1,22 +1,26 @@
-expose("addProfile",HC.JSON);
+//expose("addProfile",HC.JSON);
 function addProfile(x) {return commit("profile",x);}
 expose("addPost",HC.JSON);
-function addPost(x) {return commit("post",x);}
-function validate(entry_type,entry,meta) {
-    if (entry_type=="post") {
-        var l = entry.message;
-        if (l>0 && l<256) {return true;}
-        return false;
-    }
-    if (entry_type=="profile") {
-        return true;
-    }
-    return false;
+function addPost(x) {
+    var id = property("_id");
+    var nick = property("_agent_name");
+    var k = commit("post",x);
+    putmeta(id,k,"_post_by_"+nick);
+    return k;
 }
 expose("users",HC.JSON)
 function users() {
     debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     return getmeta(property("_id"),"users")
+}
+
+expose("get",HC.JSON)
+function get(i) {
+    debug("get")
+    if (i.what == "nick") {
+        return property("_agent_name");
+    }
+    return {}
 }
 
 function genesis() {
@@ -28,4 +32,16 @@ function genesis() {
         return true;
     }
     return true;
+}
+
+function validate(entry_type,entry,meta) {
+    if (entry_type=="post") {
+        var l = entry.message;
+        if (l>0 && l<256) {return true;}
+        return false;
+    }
+    if (entry_type=="profile") {
+        return true;
+    }
+    return false;
 }

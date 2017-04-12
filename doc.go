d2cb1156054ed61c654932e571112ca92e194989
Matthew Schutte <matthewjosef@gmail.com>
2017-04-11 18:11:17 -0700

    update doc.go

--- doc.go
+++ doc.go
@@ -162,4 +162,5 @@ However, at the moment, these apps don't exist and holochain is largely for
 developers trying to build these things for you. Check back in Q2 of 2017 for some 
 cool applications.
 */
+
 package holochain

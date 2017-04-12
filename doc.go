9d2bb9defc797e2cf1f7bfcc0ff75df594d2bafd
Matthew Schutte <matthewjosef@gmail.com>
2017-04-11 18:16:54 -0700

    update doc.go formatting

--- doc.go
+++ doc.go
@@ -85,7 +85,6 @@ Each holochain has THREE MAIN SUB-SYSTEMS
 -Source Chain
 
 -Shared Storage (Validating DHT)
-
 see http://ceptr.org/images/Holochain_Subsystems.png
 
 NUCLEUS APPLICATION ENGINE (APPLICATION)

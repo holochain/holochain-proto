5a6daf2376008db6eaa82417249e85573cdf333d
timotree3 <timorcb@gmail.com>
2017-05-24 15:02:06 -0400

    Rework and optimization of lightweight dockerfile
    Some of the things which have been removed from this image will be added
    to the dev dockerfile in my next commit.

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,9 @@ if id -nG "$USER" | grep -qw "docker"; then
 else
         docker="sudo docker"
 fi
-$docker build -f docker/Dockerfile -t metacurrency/holochain .
+# If id command is installed then use it to mimick the id, otherwise don't.
+if [ -n $(command -v id) ]; then
+        $docker build -f docker/Dockerfile -t metacurrency/holochain --build-arg uid=$(id -u) .
+else
+        $docker build -f docker/Dockerfile -t metacurrency/holochain .
+fi

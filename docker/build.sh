d1e16b766b1cb27f7a6fa0891b1616dff7b5336d
timotree3 <timorcb@gmail.com>
2017-05-24 20:59:35 -0400

    Restructure docker folder
    This will make sense for future commits.

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+name="hc-dev-tools"
+tag="jessie"
 if id -nG "$USER" | grep -qw "docker"; then
         docker="docker"
 else
@@ -6,7 +8,7 @@ else
 fi
 # If id command is installed then use it to mimick the id, otherwise don't.
 if [ -n $(command -v id) ]; then
-        $docker build -f docker/Dockerfile -t metacurrency/holochain --build-arg uid=$(id -u) .
+        $docker build -f "docker/$name/Dockerfile.$tag" -t "$name:$tag" --build-arg uid=$(id -u) .
 else
-        $docker build -f docker/Dockerfile -t metacurrency/holochain .
+        $docker build -f "docker/$name/Dockerfile.$tag" -t "$name:$tag" .
 fi

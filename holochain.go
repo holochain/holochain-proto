dd8cb1d174951ab1da997d4fff0c622c5067eef1
Eric Harris-Braun <eric@harris-braun.com>
2017-07-31 14:35:09 -0400

    version bump -> 13

--- holochain.go
+++ holochain.go
@@ -24,10 +24,10 @@ import (
 )
 
 // Version is the numeric version number of the holochain library
-const Version int = 12
+const Version int = 13
 
 // VersionStr is the textual version number of the holochain library
-const VersionStr string = "12"
+const VersionStr string = "13"
 
 // Loggers holds the logging structures for the different parts of the system
 type Loggers struct {

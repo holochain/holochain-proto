3322239aa84da19ec1f26bdc874583f957d549e5
Eric Harris-Braun <eric@harris-braun.com>
2017-08-03 22:34:42 -0400

    add hcdev web test, #246

--- cmd/hcdev/hcdev_test.go
+++ cmd/hcdev/hcdev_test.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"github.com/metacurrency/holochain/cmd"
 	. "github.com/smartystreets/goconvey/convey"
 	_ "github.com/urfave/cli"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestSetupApp(t *testing.T) {
@@ -55,5 +58,55 @@ func TestInit(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(cmd.IsFile(filepath.Join(tmpTestDir, "bar", "ui", "foo.js")), ShouldBeTrue)
 	})
+}
 
+func TestWeb(t *testing.T) {
+	tmpTestDir, err := ioutil.TempDir("", "holochain.testing.hcdev")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(tmpTestDir)
+
+	err = os.Chdir(tmpTestDir)
+	if err != nil {
+		panic(err)
+	}
+	app := setupApp()
+
+	os.Args = []string{"hcdev", "init", "foo"}
+	err = app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Chdir(filepath.Join(tmpTestDir, "foo"))
+	if err != nil {
+		panic(err)
+	}
+
+	Convey("'web' should run a webserver", t, func() {
+		os.Args = []string{"hcdev", "web"}
+
+		old := os.Stdout // keep backup of the real stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		go app.Run(os.Args)
+		time.Sleep(time.Second / 2)
+
+		outC := make(chan string)
+		// copy the output in a separate goroutine so printing can't block indefinitely
+		go func() {
+			var buf bytes.Buffer
+			io.Copy(&buf, r)
+			outC <- buf.String()
+		}()
+
+		// back to normal state
+		w.Close()
+		os.Stdout = old // restoring the real stdout
+		out := <-outC
+		So(out, ShouldContainSubstring, "on port:4141")
+		So(out, ShouldContainSubstring, "Serving holochain with DNA hash:")
+	})
 }

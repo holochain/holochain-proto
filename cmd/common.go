c2fe4fe9672618ad0e63b9385b125858753f8e81
Raymond D. Powell <rayzer42@gmail.com>
2017-08-18 11:30:31 -0700

    fixed hardcoded 10 seconds in GetUnixTimestamp_secondsFromNow

--- cmd/common.go
+++ cmd/common.go
@@ -221,7 +221,7 @@ func GetFreePort() (port int, err error) {
 }
 
 func GetUnixTimestamp_secondsFromNow(seconds int) int64 {
-	return time.Now().Add(10 * time.Second).Unix()
+	return time.Now().Add(seconds * time.Second).Unix()
 }
 func GetDuration_fromUnixTimestamp(timestamp int64) (duration time.Duration) {
 	duration = 0 * time.Second

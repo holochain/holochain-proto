32cd251428db058bf57a1e0a3ca51f212b73714e
Eric Harris-Braun <eric@harris-braun.com>
2017-09-29 17:08:48 +0200

    fixed bug where tag wasn't getting js sanitized.

--- jsribosome.go
+++ jsribosome.go
@@ -1177,7 +1177,7 @@ func NewJSRibosome(h *Holochain, zome *Zome) (n Ribosome, err error) {
 				var l string
 				l = `Hash:"` + th.H + `"`
 				if tag == "" {
-					l += `Tag:"` + th.T + `"`
+					l += `Tag:"` + jsSanatizeString(th.T) + `"`
 				}
 				if options.Load {
 					l += `,EntryType:"` + jsSanitizeString(th.EntryType) + `"`

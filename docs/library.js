6808f147d7c3e786bd477f4ff0bb073001efec0d
Raymond D. Powell <rayzer42@gmail.com>
2018-02-22 14:41:35 -0500

    WIP for Robert

--- docs/library.js
+++ docs/library.js
@@ -1,10 +1,31 @@
 function Library() {
 }
 
-Library.prototype.heading_function = function (name, parms) {
+Library.prototype.function_heading = function (name, parms) {
+    return `
+        <h2 class="function-heading">` + name + `</h2>
+    `
 }
 
-Library.prototype.heading_example = function (name) {
+Library.prototype.code_example_heading = function (name) {
+}
+
+Library.prototype.code_example_block_header = function (name, exampleName) {
+    return `
+    <pre><code class="code-example" id="code-example-` + name + `-` + exampleName + `">
+    `
+}
+
+Library.prototype.code_example_block_footer = function (name, exampleName) {
+    return `
+        </code></pre>
+    `
+}
+
+Library.prototype.function_link = function (name, exampleName) {
+    return `
+        <a href="#">` + name + `</a>
+    `
 }
 
 module.exports = new Library();

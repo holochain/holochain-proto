be8326589ef4dd75c481778cc63e0ce089adffe5
David Meister <thedavidmeister@gmail.com>
2018-05-05 15:05:48 +1000

    move bundlestart to action file

--- action.go
+++ action.go
@@ -284,39 +284,6 @@ func argErr(typeName string, index int, arg Arg) error {
 }
 
 //------------------------------------------------------------
-// StartBundle
-
-const (
-	DefaultBundleTimeout = 5000
-)
-
-type APIFnStartBundle struct {
-	timeout   int64
-	userParam string
-}
-
-func NewStartBundleAction(timeout int, userParam string) *APIFnStartBundle {
-	a := APIFnStartBundle{timeout: int64(timeout), userParam: userParam}
-	if timeout == 0 {
-		a.timeout = DefaultBundleTimeout
-	}
-	return &a
-}
-
-func (a *APIFnStartBundle) Name() string {
-	return "bundleStart"
-}
-
-func (a *APIFnStartBundle) Args() []Arg {
-	return []Arg{{Name: "timeout", Type: IntArg}, {Name: "userParam", Type: StringArg}}
-}
-
-func (a *APIFnStartBundle) Call(h *Holochain) (response interface{}, err error) {
-	err = h.Chain().StartBundle(a.userParam)
-	return
-}
-
-//------------------------------------------------------------
 // Get
 
 type APIFnGet struct {

686f0f89fe0a23904657033265c71cc6780047d0
David Meister <thedavidmeister@gmail.com>
2018-05-05 15:01:14 +1000

    move query to new action file

--- action.go
+++ action.go
@@ -453,26 +453,6 @@ func (a *APIFnVerifySignature) Call(h *Holochain) (response interface{}, err err
 }
 
 //------------------------------------------------------------
-// Query
-
-type APIFnQuery struct {
-	options *QueryOptions
-}
-
-func (a *APIFnQuery) Name() string {
-	return "query"
-}
-
-func (a *APIFnQuery) Args() []Arg {
-	return []Arg{{Name: "options", Type: MapArg, MapType: reflect.TypeOf(QueryOptions{}), Optional: true}}
-}
-
-func (a *APIFnQuery) Call(h *Holochain) (response interface{}, err error) {
-	response, err = h.Query(a.options)
-	return
-}
-
-//------------------------------------------------------------
 // Get
 
 type APIFnGet struct {
---
+++ action_query.go
@@ -0,0 +1,21 @@
+package holochain
+
+//------------------------------------------------------------
+// Query
+
+type APIFnQuery struct {
+	options *QueryOptions
+}
+
+func (a *APIFnQuery) Name() string {
+	return "query"
+}
+
+func (a *APIFnQuery) Args() []Arg {
+	return []Arg{{Name: "options", Type: MapArg, MapType: reflect.TypeOf(QueryOptions{}), Optional: true}}
+}
+
+func (a *APIFnQuery) Call(h *Holochain) (response interface{}, err error) {
+	response, err = h.Query(a.options)
+	return
+}

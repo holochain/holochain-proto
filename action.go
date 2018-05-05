7f94c7c625976d743cf31dd3fd68c7fe5f5ed336
David Meister <thedavidmeister@gmail.com>
2018-05-05 15:03:25 +1000

    move sign to action file

--- action.go
+++ action.go
@@ -397,31 +397,6 @@ func (a *APIFnGetBridges) Call(h *Holochain) (response interface{}, err error) {
 }
 
 //------------------------------------------------------------
-// Sign
-
-type APIFnSign struct {
-	data []byte
-}
-
-func (a *APIFnSign) Name() string {
-	return "sign"
-}
-
-func (a *APIFnSign) Args() []Arg {
-	return []Arg{{Name: "data", Type: StringArg}}
-}
-
-func (a *APIFnSign) Call(h *Holochain) (response interface{}, err error) {
-	var sig Signature
-	sig, err = h.Sign(a.data)
-	if err != nil {
-		return
-	}
-	response = sig.B58String()
-	return
-}
-
-//------------------------------------------------------------
 // Get
 
 type APIFnGet struct {

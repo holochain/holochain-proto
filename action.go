75abcf9dea54b85f10bc5deb50571cf114b5f6e9
David Meister <thedavidmeister@gmail.com>
2018-05-23 00:57:40 +1000

    move APIFnGetBridges to action_getbridges.go

--- action.go
+++ action.go
@@ -327,25 +327,6 @@ func (a *APIFnMakeHash) Call(h *Holochain) (response interface{}, err error) {
 }
 
 //------------------------------------------------------------
-// GetBridges
-
-type APIFnGetBridges struct {
-}
-
-func (a *APIFnGetBridges) Name() string {
-	return "getBridges"
-}
-
-func (a *APIFnGetBridges) Args() []Arg {
-	return []Arg{}
-}
-
-func (a *APIFnGetBridges) Call(h *Holochain) (response interface{}, err error) {
-	response, err = h.GetBridges()
-	return
-}
-
-//------------------------------------------------------------
 // Sign
 
 type APIFnSign struct {
